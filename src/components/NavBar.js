864e36f99c1115caaf9c92e53757123db43623c3
Michael <mdart86@hotmail.com>
2021-05-24 20:58:17 +1000

    additional content to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,42 @@
 import React from 'react'
 import "../styling/styling.scss"
-import {ContactMailIcon,GitHubIcon,Twitter} from '@material-ui/icons';
-
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import Twitter from '@material-ui/icons/Twitter';
 
 const NavBar = () => {
     return(
-        <div className="nav-bar">
+        <div className="nav-bar active">
             <div className="inner-nav-bar">
                 <div className="left-inner-nav-bar">
-                    <a href="#intro"className="logo">genius.</a>
+                    <a href="#intro"className="logo">Michael Dart</a>
                         <div className="item-container">
-                            <ContactMailIcon/>
-                            <span>My Email</span>
+                            <ContactMailIcon className="icon" />
+                            <span>mdart86@hotmail.com</span>
                         </div>
                         <div className="item-container">
-                            <GitHubIcon/>
-                            <span>GitHub Icon</span>
+                            <GitHubIcon className="icon"/>
+                            <span>mdart86</span>
                         </div>
                         <div className="item-container">
-                            <Twitter/>
-                            <span>Twitter</span>
+                            <InstagramIcon className="icon" />
+                            <span>mdart86</span>
+                        </div>
+                        <div className="item-container">
+                            <Twitter className="icon"/>
+                            <span>@MichaelDart12</span>
                         </div>
                     <a href="#portfolio">Portfolio</a>
                     <a href="#contact">Contact</a>
                 </div>
                 <h1>Hello Left</h1>
                 <div className="right-inner-nav-bar">
-                    <h1>Hello Right</h1>    
+                    <div className="hamburger">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
                 </div>            
             </div>    
         </div>
