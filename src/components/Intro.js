a57f6719247c1af3b3b3f48b1ea5b9040b43b0e5
Michael <mdart86@hotmail.com>
2021-05-26 19:48:13 +1000

    intro page and image addition

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,7 +5,11 @@ import "../styling/styling.scss"
 const Intro = () => {
     return(
         <div className="intro" id="intro">
-            <h2>Intro</h2>
+            <div className="left"></div>
+                <div className="image-container">
+                    <img src="../public/assets/me.png" alt="me-in-vancouver"/>
+                </div>
+            <div className="right"></div>
         </div>
     )
 
