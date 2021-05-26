269ea51a12228ca8c9bdd0e625e94b338adce98d
Michael <mdart86@hotmail.com>
2021-05-26 21:14:53 +1000

    inital intro layout completed

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,18 +1,42 @@
 import React from 'react'
+import { init } from 'ityped'
+
+import { useEffect, useRef } from 'react'
+
 import "../styling/styling.scss"
 
 
+
+
 const Intro = () => {
+
+    const textRef = useRef()
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: false,
+            backDelay: 2000,
+            strings: ["Studying Full Stack Web Development", "Evolving into a Junior developer"]
+        })
+    },[])
+
     return(
         <div className="intro" id="intro">
-            <div className="left"></div>
+            <div className="left-intro">
                 <div className="image-container">
-                    <img src="../public/assets/me.png" alt="me-in-vancouver"/>
+                    <img src="assets/me.png" alt="me-in-vancouver"/>
                 </div>
-            <div className="right"></div>
+            </div>
+            <div className="right-intro">
+                <div className="intro-text">
+                    <h2>Hello there, I am,</h2>
+                    <h1>Michael Dart</h1>
+                    <h3>I am a...<span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">Portfolio... CLICK HERE</a>
+            </div>
         </div>
     )
-
 }
 
 export default Intro
