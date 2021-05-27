6b0f7798f515677dc40895414b0712ad823e44e2
Michael <mdart86@hotmail.com>
2021-05-27 19:54:00 +1000

    portfolio page started

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,7 +4,20 @@ import "../styling/styling.scss"
 const Portfolio = () => {
     return(
         <div className="portfolio" id="portfolio">
-            <h2>Portfolio Page</h2>
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Original Portfolio Website</li>
+                <li>Terminal Application</li>
+                <li>Two Sided Marketplace</li>
+            </ul>
+            <div className="portfolio-container">
+                <img src="assets/mnm.png" alt="michaelandmason"/>
+                <h3>Original Portfolio Website</h3>
+            </div>
+            <div className="portfolio-container">
+                <img src="assets/mnm.png" alt="michaelandmason"/>
+                <h3>Original Portfolio Website</h3>
+            </div>
         </div>
     )
 
