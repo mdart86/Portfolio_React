d3d3e852a39b7921f0ac74b32372652a61967dd0
Michael <mdart86@hotmail.com>
2021-05-30 13:35:03 +1000

    updates to contact area

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,32 +1,39 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { SiMinutemailer, SiGithub, SiLinkedin } from "react-icons/si";
+// import { Button } from 'react-bootstrap';
 import "../styling/styling.scss"
 
 
 const Contact = () => {
-    const [message, setMessage] = useState(false)
+    // const [message, setMessage] = useState(false)
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setMessage(true)
-    }
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     setMessage(true)
+    // }
 
     return(
         <div className="contact" id="contact">
-            <div className="left-contact"></div>
+            <div className="left-contact">
                 <img src="assets/diana-polekhina-1ixT36dfuSQ-unsplash.jpg" alt="new-message-icon"/>
-            <div className="right-contact">
-                <h2>Send me a message.</h2>
-                    <form onSubmit={handleSubmit}>
-                        <input type="email" placeholder="Enter email address"/>
-                        <textarea placeholder="Type your message here..."></textarea>
-                        <button type="submit">Send</button> 
-                        {/* add logic that will allow these fields not to be empty */}
-                        { message && <span>Thank you for your message, I will reply soon!</span> }
-                    </form>
             </div>
+                <div className="right-contact">
+                    <h2>Lets connect and do some great things together</h2>
+                    <a href="mailto: mdart86@hotmail.com" className="contact-me-button">
+                        <p className="pr-2">Email</p>
+                        <SiMinutemailer className="contact-me-text" />
+                    </a>
+                    <a href="https://github.com/mdart86" target="_blank" rel="noopener noreferrer" className="contact-me-button">
+                        <p className="pr-2">GitHub</p>
+                        <SiGithub className="contact-me-text"/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/michael-dart-537a3b57/" target="_blank" rel="noopener noreferrer" className="contact-me-button">
+                        <p className="pr-2">LinkedIn</p>
+                        <SiLinkedin className="contact-me-text"/>
+                    </a>
+                </div>
         </div>
     )
-
 }
 
 export default Contact
