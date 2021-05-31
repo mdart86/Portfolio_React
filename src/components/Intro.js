87a2fecdef9e2d19ca3f09204d8797e80dbc57b2
Michael <mdart86@hotmail.com>
2021-05-31 21:41:50 +1000

    updates to fonts

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,25 +1,10 @@
 import React from 'react'
-import { init } from 'ityped'
-
-import { useEffect, useRef } from 'react'
-
 import "../styling/styling.scss"
 
 
 
 
 const Intro = () => {
-
-    const textRef = useRef()
-
-    useEffect(() => {
-        init(textRef.current, {
-            showCursor: false,
-            backDelay: 2000,
-            strings: ["Studying Full Stack Web Development", "Evolving into a Junior developer"]
-        })
-    },[])
-
     return(
         <div className="intro" id="intro">
             <div className="left-intro">
@@ -28,11 +13,7 @@ const Intro = () => {
                 </div>
             </div>
             <div className="right-intro">
-                <div className="intro-text">
-                    <h2>Hello there, I am,</h2>
-                    <h1>Michael Dart</h1>
-                    <h3>I am a...<span ref={textRef}></span></h3>
-                </div>
+                <h1>Intro Page</h1>
                 <a href="#portfolio">Portfolio... CLICK HERE</a>
             </div>
         </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import "../styling/styling.scss"
+import { init } from 'ityped'
+
+import { useEffect, useRef } from 'react'
 
 const NavBar = ( {menuOpen, setMenuOpen} ) => {
+    
+    const textRef = useRef()
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: false,
+            backDelay: 2000,
+            strings: ["studying Full-Stack Web Development", "evolving into a Junior Web Developer"]
+        })
+    },[])
+
     return(
         <div className={"nav-bar " + (menuOpen && "active")}>
             <div className="inner-nav-bar">
                 <div className="left-inner-nav-bar">
                     <a href="#intro"className="logo">Michael Dart</a>
+                    <div className="intro-text">
+                      <h2 className="active">... <span ref={textRef}></span></h2>
+                    </div>
                 </div>
                 <div className="right-inner-nav-bar">
                     <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)} >
