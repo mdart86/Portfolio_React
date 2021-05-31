297baf217df7882a4cbd687e2f33fa81d063aa4c
Michael <mdart86@hotmail.com>
2021-05-31 21:53:04 +1000

    updates to styling for intro section incl image and paragraph

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -13,7 +13,10 @@ const Intro = () => {
                 </div>
             </div>
             <div className="right-intro">
-                <h1>Intro Page</h1>
+                <h1>Welcome</h1>
+                    <div className="intro-div">
+                        <p>From a young age I have been an explorer, when some people read novels, I read at Atlases. As I grew older and started to wonder what it would be like to see those places, It was time to throw away the Atlas! I began travelling in 1997 on my first family holiday to North America and Europe. Since that first trip, I was hooked! I have meet many new people and made some great friends along the way. Years went by, careers changed, even getting a job in Travel, I also lived in another country for a short period of time. Come just recently, I wanted to try something new, it's in my nature to learn and grow. I quote from an boss and friend: "You'll enjoy your new course, just be the sponge that you are!". That is when I decided to venture on a new path to learn about Web Development. There have been many challenges throughout my life and at the end of each challenge, I believe I have come out a stronger person. I am keen to progress in this new and ever changing world. I look forward to tackling the new challenges that lie ahead (or do I? Ha ha). </p>            
+                    </div>
                 <a href="#portfolio">Portfolio... CLICK HERE</a>
             </div>
         </div>
